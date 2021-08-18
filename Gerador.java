409973c648eaeffedb89971c74f4493fca8e30ae
Marcelo Veiga Neves <marcelo.veiga@gmail.com>
2021-08-18 20:25:32 -0300

    Modifica Gerador para demonstrar a funcionalidade do | "pipe"

--- Gerador.java
+++ Gerador.java
@@ -1,14 +1,23 @@
 import java.util.Random;
 
 public class Gerador {
-    public static void main(String args[]) {
-        int min = 1;
-        int max = 100;
-        for (int i = 0; i < 5000; i++) {
+    public static void main(String args[]) throws InterruptedException {
+        if (args.length < 3) {
+            System.out.println("Usage: java Gerador <min> <max> <quantidade>");
+            return;
+        }
+
+        int min = Integer.parseInt(args[0]);
+        int max = Integer.parseInt(args[1]);
+        int quantidade = Integer.parseInt(args[2]);
+
+        for (int i = 0; i < quantidade; i++) {
             //int num = min + (int)(Math.random()*max);            
             Random rand = new Random();
             int num = min + rand.nextInt(max);
             System.out.print(num + " ");
+            System.err.println("Gerou o numero " + num);
+            Thread.sleep(2000);
         }
     }
 }
--- Media.java
+++ Media.java
@@ -2,12 +2,18 @@ import java.util.Scanner;
 
 public class Media {
     public static void main(String args[]) {
+
         Scanner sc = new Scanner(System.in);
         int soma = 0;
-        for (int i = 0; i < 5000; i++) {
+        int count = 0;
+
+        while(sc.hasNext()) {
             int num = sc.nextInt();
+            System.err.println("Li o numero " + num);
             soma += num;
+            count++;
         }
-        System.out.println("Media: " + soma/(double)5000);
+
+        System.out.println("Media: " + soma/(double)count);
     }
 }
